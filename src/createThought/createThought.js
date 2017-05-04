ea79428e53d9de7a68422511c3a77516f0f08916
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-04 10:31:40 -0600

    createThought test passing

--- src/createThought/createThought.js
+++ src/createThought/createThought.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 export default class CreateThought extends Component {
+  constructor(){
+    super();
+    this.state = {
+      title: '',
+      body: ''
+    }
+  }
+
+submitThought(){
+  this.props.createThought(this.state)
+  this.setState({
+      title: '',
+      body: ''
+    }
+  )
+}
   render() {
     return(
-      <div>CreateThought</div>
+      <div>
+        <h1>CreateThought</h1>
+        <input value={this.state.title}onChange={(e)=>this.setState({title:e.target.value})} />
+        <input value={this.state.body}onChange={(e)=>this.setState({body:e.target.value})} />
+        <button onClick={(e)=>{this.submitThought()}}>Submit</button>
+      </div>
     );
+
   }
 
 }
